28523a9253921eadb975d807272a197ba3490364
Psmithcode <peytonsmith53@gmail.com>
2023-06-28 18:58:58 -0500

    added autocomplete and changed title/removed vite default scaffolding

--- roadTripAI/src/App.jsx
+++ roadTripAI/src/App.jsx
@@ -1,12 +1,13 @@
-import { useState } from 'react'
+import { useState } from "react";
+import PlacesAutocomplete from "./components/PlacesAutocomplete";
 
 function App() {
-
   return (
     <>
-     <h1>Road Trip AI</h1>
+      <h1>Road Trip AI</h1>
+      <PlacesAutocomplete />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
