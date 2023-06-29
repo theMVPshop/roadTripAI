250c0cdaa144773c376dc19714a4cb5835f2becf
Nathan <nathan.guevara@gmail.com>
2023-06-28 19:04:21 -0500

    testing development branch

--- roadTripAI/src/App.jsx
+++ roadTripAI/src/App.jsx
@@ -5,6 +5,9 @@ function App() {
   return (
     <>
      <h1>Road Trip AI</h1>
+     {/* the h2 below is for me to test to see what happens when i push to dev branch */}
+     <h2>This is a test</h2>
+     
     </>
   )
 }
