96f5e4607b682f3366f037770e692e4b586558b8
Caroline-Patrick <carolinespatrick@gmail.com>
2023-07-13 18:54:45 -0500

    updated npm package with dotenv & sent itineraryprops to Leaflet map

--- roadTripAI/src/components/GetItinerary.jsx
+++ roadTripAI/src/components/GetItinerary.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
+import LeafletMap from "./LeafletMap";
 const secretKey = import.meta.env.VITE_SECRET_KEY;
 
-import { itineraryDummyData } from "../assets/sampleItineraryData";
 
 const prompt =  "I'm planning a roadtrip, leaving on August 8 from Austin and arriving on August 15 at New York City.  I am driving a fairly direct route.  Make me an itinerary of interesting stops along the way. I want to go to one interesting place per day, and on the first day, the interesting place should not be in my starting city. Each interesting place should be at least 2 hours but no more than 8 hours away from the previous interesting place.  Give me an array of objects, each object representing a day of the road trip. I want to know the date as YYYY-MM-DD (date), longitude (lng), latitude (lat), name of the stop (name), a description of the stop (desc), the city closest to the stop as 'city, state abbreviation' (city), the drive time from the previous stop as a decimal (time), and the average historical temperature for the stop on the date we will arrive (temp). Please do not provide any additional text outside of the array"
 
@@ -43,12 +43,8 @@ const GetItinerary = () => {
 
   return (
     <div>
-      <h1>something</h1>
-      <ul>
-        {itinerary.map((data) => {
-          return <li key={data.city}>{data.city}</li>;
-        })}
-      </ul>
+      <LeafletMap itineraryData={itinerary} />
+
     </div>
   );
 };
