991f0939df32db0b67f3bf64a76363f97720f331
edward-vera <103536879+edward-vera@users.noreply.github.com>
2023-07-12 20:48:39 -0500

    ignore this push

--- roadTripAI/src/App.jsx
+++ roadTripAI/src/App.jsx
@@ -1,18 +1,11 @@
-import { useState } from "react";
-import 'leaflet/dist/leaflet.css'
-
-import "./styles/App.css";
+import "./styles/App.css"
 
 import MenuBar from './components/MenuBar'
-import MainMenu from "./components/MainMenu";
-import LeafletMap from "./components/LeafletMap";
 
 function App() {
   return (
-    <div className="App">
+    <div className="background">
     <MenuBar />
-    <LeafletMap/>
-    <Results/>
     </div>
   )
 }
