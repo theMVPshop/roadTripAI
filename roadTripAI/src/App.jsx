115a45f699da40785e794bab3323de656d0ae02b
edward-vera <103536879+edward-vera@users.noreply.github.com>
2023-07-13 19:08:56 -0500

    i still dont know whats going on

--- roadTripAI/src/App.jsx
+++ roadTripAI/src/App.jsx
@@ -4,21 +4,13 @@ import MenuBar from './components/MenuBar'
 import MainMenu from "./components/MainMenu";
 import LeafletMap from "./components/LeafletMap";
 import GetItinerary from "./components/GetItinerary";
-import "./styles/App.css"
-
-import MenuBar from './components/MenuBar.jsx'
-import LeafletMap from './components/LeafletMap.jsx'
-import MainMenu from './components/MainMenu.jsx'
 
 function App() {
   return (
     <>
     <MenuBar />
-     <h1>Road Trip AI</h1>
-     <h2>Bottom Text</h2>
      <LeafletMap />
      <GetItinerary />
-      <MenuBar />
       <MainMenu />
       <LeafletMap />
     </>
