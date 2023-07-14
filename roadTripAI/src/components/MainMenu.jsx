c929c28d802fd6ea1cab5207baabe3f87d3fe84d
SamSvetlik <sam.svetlik@gmail.com>
2023-07-13 19:51:38 -0500

    changed inputs for start and end date with react-date-picker package, unstyled

--- roadTripAI/src/components/MainMenu.jsx
+++ roadTripAI/src/components/MainMenu.jsx
@@ -1,10 +1,25 @@
 import React, { useState } from "react";
 import PlacesAutoComplete from "./PlacesAutocomplete";
+import DatePicker from 'react-date-picker'
 import "../styles/MainMenu.css";
+import 'react-date-picker/dist/DatePicker.css'; // Default styling for DatePicker
+import 'react-calendar/dist/Calendar.css';
 
 export default function MainMenu() {
   const [startLocation, setStartLocation] = useState("");
   const [endLocation, setEndLocation] = useState("");
+  const [startDate, setStartDate] = useState(new Date())
+  const [endDate, setEndDate] = useState(new Date())
+
+  function formatDate(date) {
+    // The current start dates are Date() objects, and we need to convert them to the dd-mm-yyyy format.
+    const year = date.getFullYear(); // Get full year
+    const month = String(date.getMonth() + 1).padStart(2, '0'); // Get month with leading zero if necessary
+    const day = String(date.getDate()).padStart(2, '0'); // Get day with leading zero if necessary
+  
+    return `${day}-${month}-${year}`; // Format the date as "dd-mm-yyyy"
+  }
+
   return (
     <div className="mainMenu">
       <div className="mainMenuInputContainer ">
@@ -19,11 +34,7 @@ export default function MainMenu() {
 
         <section className="startDateContainer inputContainer">
           <h2 className="startDateTitle">Start Date:</h2>
-          <PlacesAutoComplete
-            className="startLocationInput"
-            location={startLocation}
-            setLocation={setStartLocation}
-          />
+          <DatePicker onChange={setStartDate} value={startDate}/>
         </section>
 
         <section className="endLocationContainer inputContainer">
@@ -37,11 +48,7 @@ export default function MainMenu() {
 
         <section className="endDateContainer inputContainer">
           <h2 className="endDateTitle">Start Location:</h2>
-          <PlacesAutoComplete
-            className="startLocationInput"
-            location={startLocation}
-            setLocation={setStartLocation}
-          />
+          <DatePicker onChange={setEndDate} value={endDate}/>
         </section>
 
         <button className="mainMenuSubmitButton">Submit</button>
