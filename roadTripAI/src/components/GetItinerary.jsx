638cdd5ec3e3a5cd993b30417b39b1fc9cc791ce
Caroline-Patrick <carolinespatrick@gmail.com>
2023-07-17 18:16:28 -0500

    added useEffect to call fetchItinerary on page load

--- roadTripAI/src/components/GetItinerary.jsx
+++ roadTripAI/src/components/GetItinerary.jsx
@@ -2,19 +2,19 @@ import React, { useEffect, useState } from "react";
 import LeafletMap from "./LeafletMap";
 const secretKey = import.meta.env.VITE_SECRET_KEY;
 
-
-const prompt =  "I'm planning a roadtrip, leaving on August 8 from Austin and arriving on August 15 at New York City.  I am driving a fairly direct route.  Make me an itinerary of interesting stops along the way. I want to go to one interesting place per day, and on the first day, the interesting place should not be in my starting city. Each interesting place should be at least 2 hours but no more than 8 hours away from the previous interesting place.  Give me an array of objects, each object representing a day of the road trip. I want to know the date as YYYY-MM-DD (date), longitude (lng), latitude (lat), name of the stop (name), a description of the stop (desc), the city closest to the stop as 'city, state abbreviation' (city), the drive time from the previous stop as a decimal (time), and the average historical temperature for the stop on the date we will arrive (temp). Please do not provide any additional text outside of the array"
+const prompt =
+  "I'm planning a roadtrip, leaving on August 8 from Austin and arriving on August 15 at New York City.  I am driving a fairly direct route.  Make me an itinerary of interesting stops along the way. I want to go to one interesting place per day, and on the first day, the interesting place should not be in my starting city. Each interesting place should be at least 2 hours but no more than 8 hours away from the previous interesting place.  Give me an array of objects, each object representing a day of the road trip. I want to know the date as YYYY-MM-DD (date), longitude (lng), latitude (lat), name of the stop (name), a description of the stop (desc), the city closest to the stop as 'city, state abbreviation' (city), the drive time from the previous stop as a decimal (time), and the average historical temperature for the stop on the date we will arrive (temp). Please do not provide any additional text outside of the array";
 
 const GetItinerary = () => {
   const [itinerary, setItinerary] = useState([]);
+  const [error, setError] = useState(null);
 
   const fetchItinerary = () => {
     const url = "https://api.openai.com/v1/chat/completions";
     fetch(url, {
       method: "POST",
       headers: {
-        Authorization:
-          secretKey,
+        Authorization: secretKey,
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
@@ -29,22 +29,29 @@ const GetItinerary = () => {
         return response.json();
       })
       .then((data) => {
-        const parsedContent = JSON.parse(data.choices[0].message.content)
-   
-        setItinerary(parsedContent)
-   
-      console.log( itinerary)
+        const parsedContent = JSON.parse(data.choices[0].message.content);
+        setItinerary(parsedContent);
+        
+
+      })
+      .catch((error) => {
+        setError(error);
+        console.error("Error:", error);
       });
   };
 
   useEffect(() => {
-    fetchItinerary();
+    console.log(itinerary);
   }, [itinerary]);
+
+    // call fetchItinerary after component loads
+    useEffect(() => {
+      fetchItinerary();
+    }, []);
 
   return (
     <div>
-      <LeafletMap itineraryData={itinerary} />
-
+      <LeafletMap itinerary={itinerary} />
     </div>
   );
 };
--- roadTripAI/src/components/LeafletMap.jsx
+++ roadTripAI/src/components/LeafletMap.jsx
@@ -1,6 +1,7 @@
 import { MapContainer, TileLayer, Marker, Popup, Polyline } from 'react-leaflet'
+import {useState, useEffect} from 'react';
 
-const LeafletMap = (props) => {
+const LeafletMap = ({itinerary}) => {
 
     // MapContainer takes a prop called center. This should be an array with 2 entries, a lat and lng.
     // Default is the geographic center of the US
@@ -10,125 +11,22 @@ const LeafletMap = (props) => {
     // Polyfill component draws a straight line between each stop on our roadtrip.
     // It takes in one prop, positions.
     // Second prop, pathOptions, is hard coded and only changes the color of the line.
-
-    // sampleTrip is just here for testing. Delete later.
-    const sampleTrip = [
-      {
-      "date": "2023-06-23",
-      "lng": -76.6132,
-      "lat": 39.2904,
-      "name": "Gettysburg National Military Park",
-      "desc": "Explore the battlefield and learn about the historic Civil War battle that took place in 1863.",
-      "city": "Gettysburg, PA",
-      "time": 0,
-      "temp": 75
-      },
-      {
-      "date": "2023-06-24",
-      "lng": -77.0369,
-      "lat": 38.9072,
-      "name": "Washington, D.C.",
-      "desc": "Visit the iconic landmarks like the White House, Lincoln Memorial, and the National Mall.",
-      "city": "Washington, D.C.",
-      "time": 2.5,
-      "temp": 82
-      },
-      {
-      "date": "2023-06-25",
-      "lng": -82.4584,
-      "lat": 35.047,
-      "name": "Great Smoky Mountains National Park",
-      "desc": "Experience the beauty of the most visited national park in the United States, known for its diverse wildlife and stunning landscapes.",
-      "city": "Gatlinburg, TN",
-      "time": 6,
-      "temp": 72
-      },
-      {
-      "date": "2023-06-26",
-      "lng": -86.7816,
-      "lat": 36.1627,
-      "name": "Nashville, TN",
-      "desc": "Immerse yourself in the country music scene and enjoy live performances in famous venues like the Grand Ole Opry.",
-      "city": "Nashville, TN",
-      "time": 4,
-      "temp": 88
-      },
-      {
-      "date": "2023-06-27",
-      "lng": -90.0715,
-      "lat": 29.9511,
-      "name": "New Orleans, LA",
-      "desc": "Experience the vibrant culture, delicious cuisine, and lively music scene that make New Orleans unique.",
-      "city": "New Orleans, LA",
-      "time": 7,
-      "temp": 85
-      },
-      {
-      "date": "2023-06-28",
-      "lng": -94.5786,
-      "lat": 39.0997,
-      "name": "Kansas City, MO",
-      "desc": "Explore the jazz and barbecue capital, and visit attractions like the National WWI Museum and the Nelson-Atkins Museum of Art.",
-      "city": "Kansas City, MO",
-      "time": 10,
-      "temp": 90
-      },
-      {
-      "date": "2023-06-29",
-      "lng": -104.9903,
-      "lat": 39.7392,
-      "name": "Denver, CO",
-      "desc": "Enjoy the outdoor activities and stunning landscapes of the Rocky Mountains, along with the vibrant urban scene in Denver.",
-      "city": "Denver, CO",
-      "time": 9,
-      "temp": 77
-      },
-      {
-      "date": "2023-06-30",
-      "lng": -111.891,
-      "lat": 40.7608,
-      "name": "Salt Lake City, UT",
-      "desc": "Visit the beautiful Salt Lake Temple and explore the nearby natural wonders like Great Salt Lake and Antelope Island.",
-      "city": "Salt Lake City, UT",
-      "time": 8,
-      "temp": 85
-      },
-      {
-      "date": "2023-07-01",
-      "lng": -112.074,
-      "lat": 36.094,
-      "name": "Grand Canyon National Park",
-      "desc": "Witness the breathtaking beauty of one of the world's most famous natural wonders, the Grand Canyon.",
-      "city": "Grand Canyon Village, AZ",
-      "time": 7,
-      "temp": 92
-      },
-      {
-      "date": "2023-07-02",
-      "lng": -115.172,
-      "lat": 36.1146,
-      "name": "Las Vegas, NV",
-      "desc": "Experience the glitz and glamour of the famous Las Vegas Strip, with its world-class casinos, shows, and entertainment.",
-      "city": "Las Vegas, NV",
-      "time": 5,
-      "temp": 107
-      },
-      {
-      "date": "2023-07-03",
-      "lng": -118.2437,
-      "lat": 34.0522,
-      "name": "Los Angeles, CA",
-      "desc": "Reach the final destination of your road trip and explore the many attractions of the entertainment capital of the world.",
-      "city": "Los Angeles, CA",
-      "time": 5,
-      "temp": 85
-      }
-      ]
+   
     // lineOfTravel will map over the array of trips to create an array to pass to Polyfill as positions prop.
     // >>>> Needs to be made with useState, so that the component will re-render when data changes, with useEffect call.
-    const lineOfTravel = sampleTrip.map((stop) => {
-      return [stop.lat, stop.lng]
-    })
+    // const lineOfTravel = sampleTrip.map((stop) => {
+    //   return [stop.lat, stop.lng]
+    // })
+    const [lineOfTravel, setLineOfTravel]= useState([]);
+    
+    useEffect(()=> {
+      console.log(itinerary)
+     
+     if( itinerary && itinerary.length ) {
+      setLineOfTravel(itinerary.map(stop => [stop.lat, stop.lng]));
+     }
+
+    }, [itinerary])
 
     // The following code changes the default zoom of the MapContainer
     // depending on the size of the user's screen
@@ -151,7 +49,7 @@ const LeafletMap = (props) => {
         {/* The following map() returns a marker for every stop, with a
         Popup showing the name and description.
         sampleTrip will need to be changed to the state array */}
-        {sampleTrip.map((stop) => {
+        {itinerary && itinerary.length >0 && itinerary.map((stop) => {
           return (
             <Marker position={[stop.lat, stop.lng]}>
               <Popup>
